3f8787cd025d35b7f2a679778cb8cb2732880518
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-06 13:33:27 -0700

    adding to hexagram 1 and testing single hexagram outline

--- src/scenes/SingleHexagram.js
+++ src/scenes/SingleHexagram.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 export default function SingleHexagram({navigation, route}) {
   return (
-    <View>
-      <Text>{route.params.hexagram.names[0]}</Text>
-    </View>
+    <ScrollView style={{marginHorizontal: 30}}>
+      <Text>{route.params.hexagram.names[0]} {"\n"}</Text>
+      <Text style={{fontWeight: 'bold'}}>{route.params.hexagram.tagLine} {"\n"}</Text>
+      <Text>{route.params.hexagram.overview}</Text>
+    </ScrollView>
   )
 }
