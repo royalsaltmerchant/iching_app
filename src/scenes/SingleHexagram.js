53f0de9473b8fcd9a814f154c806862721ef5c3b
Julian ranieri <jumpingafterrain@gmail.com>
2022-02-28 15:34:46 -0800

    lines info detailed

--- src/scenes/SingleHexagram.js
+++ src/scenes/SingleHexagram.js
@@ -2,11 +2,25 @@ import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
 
 export default function SingleHexagram({navigation, route}) {
+
+  function renderLines() {
+    const lines = Object.values(route.params.hexagram.linesOverview).map((line, index) => (
+      <View>
+        <Text>Line {index + 1} <Text style={{fontWeight: 'bold'}}>{line.type}</Text>: {"\n"}</Text>
+        <Text style={{fontWeight: 'bold'}}>{line.title} {"\n"}</Text>
+        <Text>{line.overview} {"\n"}</Text>
+      </View>
+    ))
+    return lines
+  }
+
   return (
     <ScrollView style={{marginHorizontal: 30}}>
-      <Text>{route.params.hexagram.names[0]} {"\n"}</Text>
+      <Text style={{fontSize: 20, textAlign: 'center'}}>{route.params.hexagram.names[0]} {"\n"}</Text>
       <Text style={{fontWeight: 'bold'}}>{route.params.hexagram.tagLine} {"\n"}</Text>
-      <Text>{route.params.hexagram.overview}</Text>
+      <Text>{route.params.hexagram.overview} {"\n"}</Text>
+      <Text style={{fontWeight: 'bold'}}>Lines Overview: {"\n"}</Text>
+      {renderLines()}
     </ScrollView>
   )
 }
